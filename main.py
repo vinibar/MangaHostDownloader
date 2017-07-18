e2c930de8254acfa45dbe8d9dae0ed78410f6bc4
vinibar <vinicius21@gmail.com>
2017-07-18 15:50:01 -0300

    - structuring project

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-
-import urllib.request ,urllib.error, urllib
-from bs4 import BeautifulSoup as BS
-from operator import itemgetter
+from manga_downloader import MangaHostParser
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -17,38 +14,6 @@ def choose_manga():
         choice = input("Type the name of a manga you want: ")
     return choice
 
-def search_for(title):
-    SEARCH_URL = 'https://mangahost.net/find/'
-    title.replace('\s', '+')
-
-    req = urllib.request.Request(SEARCH_URL + title, headers=hdr)
-    resource = urllib.request.urlopen(req)
-    soup = BS(resource.read(), "lxml")
-    search_results = soup.find_all("h3", {"class":"entry-title"})
-
-    manga_list = []
-    for item in search_results:
-        manga_list.append({'title':item.find('a').get('title'),
-                           'url':item.find('a').get('href')})
-
-    return manga_list
-
-def get_issues_list(url):
-    req = urllib.request.Request(url, headers=hdr)
-    resource = urllib.request.urlopen(req)
-    soup = BS(resource.read(), "lxml")
-    ul = soup.find("ul", {"class":"list_chapters"})
-    a_list = ul.find_all('a')
-
-    issues_list = []
-    for a in a_list:
-        s = BS(a.get('data-content'), "lxml")
-        a2 = s.find('a')
-        issues_list.append({"title":a.get('data-original-title'),
-                            "url":a2.get('href'),
-                            "id":int(a.get('id'))})
-    return sorted(issues_list, key=itemgetter('id'))
-
 if __name__ == '__main__':
     print(' ---------------- MangaHost Downloader ----------------')
     print('* This program was created just for learning purposese.')
@@ -56,7 +21,9 @@ if __name__ == '__main__':
     print('\n\n')
 
     manga_name = str(choose_manga())
-    results = search_for(manga_name)
+
+    parser = MangaHostParser()
+    results = parser.search_for(manga_name)
     if not results:
         print("Sorry, there's no results for this search")
         exit(0)
@@ -77,7 +44,7 @@ if __name__ == '__main__':
             print("Invalid option. Try again.")
         break
 
-    for issue in get_issues_list(results[int(choice)]["url"]):
+    for issue in parser.get_issues_list(results[int(choice)]["url"]):
         print(issue)
 
 
--- manga_downloader.py
+++ manga_downloader.py
@@ -1,118 +1,92 @@
 # -*- coding: utf-8 -*-
-""" Manga Downloader
-Download any manga from MangaHost (www.mangahost.net)
-
-
-Attributes:
-    hdr (list): HTTP Headers to access the links since there are some
-        restriction rules imposed by the host's firewall
-
-    base_url (string): Just a sample URL used for test purpose
-
-Todo:
-    * List all the mangas avaliable in the MangaHost.net
-    * DOwnload complete collections or specific issues in ZIP
-"""
-
-
 import urllib.request ,urllib.error, urllib
 import zipfile
 import os
-from bs4 import BeautifulSoup as BS
 import re
+from operator import itemgetter
+from bs4 import BeautifulSoup as BS
 
-hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+class MangaHostParser():
+
+    HDR = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
 
-base_url = "https://mangahost.net/manga/monster-mh40618/1"
+    SEARCH_URL = 'https://mangahost.net/find/'
 
+    def _get_html(self, url):
+        req = urllib.request.Request(url, headers=MangaHostParser.HDR)
+        resource = urllib.request.urlopen(req)
+        return resource.read()
 
+    def search_for(self, title):
+        title.replace('\s', '+')
+        soup = BS(self._get_html(MangaHostParser.SEARCH_URL + title, headers=MangaHostParser.HDR), "lxml")
+        search_results = soup.find_all("h3", {"class": "entry-title"})
 
-def get_issues_from_url(url):
-    """Self-explanatory name
+        manga_list = []
+        for item in search_results:
+            manga_list.append({'title': item.find('a').get('title'),
+                               'url': item.find('a').get('href')})
+        return manga_list
 
-    Args:
-        url (str): The base URL for the manga
-    Returns:
-        list: links to the first page of all issues
+    def get_issues_list(self, url):
+        soup = BS(self._get_html(url), "lxml")
+        ul = soup.find("ul", {"class": "list_chapters"})
+        a_list = ul.find_all('a')
 
-    """
-    req = urllib.request.Request(base_url, headers=hdr)
-    resource = urllib.request.urlopen(req)
-    soup = BS(resource.read(), "lxml")
+        issues_list = []
+        for a in a_list:
+            s = BS(a.get('data-content'), "lxml")
+            a2 = s.find('a')
+            issues_list.append({"title": a.get('data-original-title'),
+                                "url": a2.get('href'),
+                                "id": int(a.get('id'))})
+        return sorted(issues_list, key=itemgetter('id'))
 
-    splitted_url = url.split("/")
-    issues = []
+    def get_pages_from_url(self, url):
+        links = re.search("(var images \= \[)(.+)(\])", self._get_html(url).decode('utf-8')).group(2)
+        links = links.replace('"', '').split(",")
 
-    for item in soup.find_all('option'):
-        try:
+        i = 0
+        while i < len(links):
+            links[i] = re.search(r'(?<=src=\').*?(?=\')', links[i]).group(0)
+            i+=1
+        return links
 
-            splitted_url[-1] = item['id']
-            issues.append("/".join(splitted_url))
-        except KeyError as e:
+    def get_filename(self, url):
+        splitted_url = url.split("/")
+        return splitted_url[-1]
+
+    def download_url(self, url):
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-Agent', MangaHostParser.hdr['User-Agent'])]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(url, self.get_filename(url))
+
+    def download_issue_none(self, url):
+        for page in self.get_pages_from_url(url):
+            self.download_url(page)
+
+    def download_issue_zip(self, url):
+        names = []
+        zip = zipfile.ZipFile('teste.zip', 'a')
+        for page in self.get_pages_from_url(url):
+            self.download_url(page)
+            names.append(self.get_filename(page))
+
+        for name in names:
+            zip.write(name)
+            os.remove(name)
+        zip.close()
+
+    def download_issue(self, url, mode=None):
+        if mode == None:
+            self.download_issue_none(url)
+        elif mode == 'zip':
+            self.download_issue_zip(url)
+        else:
             pass
-
-    return sorted(issues)
-
-
-def get_pages_from_url(url):
-    """ Get all the pages from a single issue URL
-
-    Args:
-        url (str): the URL of the issue
-
-    Returns:
-        list: links to all pages
-
-    """
-    req = urllib.request.Request(base_url, headers=hdr)
-    resource = urllib.request.urlopen(req)
-    links = re.search("(var images \= \[)(.+)(\])", resource.read().decode('utf-8')).group(2)
-    links = links.replace('"', '').split(",")
-
-    i = 0
-    while i < len(links):
-        links[i] = re.search(r'(?<=src=\').*?(?=\')', links[i]).group(0)
-        i+=1
-    return links
-
-def get_filename(url):
-    splitted_url = url.split("/")
-    return splitted_url[-1]
-
-def download_url(url):
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-Agent', hdr['User-Agent'])]
-    urllib.request.install_opener(opener)
-    urllib.request.urlretrieve(url, get_filename(url))
-
-def download_issue_none(url):
-    for page in get_pages_from_url(url):
-        download_url(page)
-
-def download_issue_zip(url):
-    names = []
-    zip = zipfile.ZipFile('teste.zip', 'a')
-    for page in get_pages_from_url(base_url):
-        download_url(page)
-        names.append(get_filename(page))
-
-    for name in names:
-        zip.write(name)
-        os.remove(name)
-    zip.close()
-
-def download_issue(url, mode=None):
-    if mode == None:
-        download_issue_none(url)
-    elif mode == 'zip':
-        download_issue_zip(url)
-    else:
-        pass
-
-
-#print(get_issues_from_url(base_url))
